e71eb85ef4b5283e45c5dcd18b5227942b2727a8
yingtianhao <149553125+yingtianhao@users.noreply.github.com>
2025-07-17 16:11:23 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,19 +74,10 @@ export default function Home({
             textAlign: 'center',
           }}
         >
-          Open-source monitoring and status page powered by{' '}
-          <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
-            Uptimeflare
-          </a>{' '}
-          and{' '}
-          <a href="https://www.cloudflare.com/" target="_blank">
-            Cloudflare
+          (c) Kelu 2023-永远 Powered by{' '}
+          <a href="https://our.kelu.one" target="_blank">
+            Kelu
           </a>
-          , made with ❤ by{' '}
-          <a href="https://github.com/lyc8503" target="_blank">
-            lyc8503
-          </a>
-          .
         </Text>
       </main>
     </>
